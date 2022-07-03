e6bfb38e47b47992b9830621cfedee832ba28fe7
Seong Yong-ju <sei40kr@gmail.com>
2022-07-03 19:16:08 +0900

    fix(config): nvim-qt init & config

--- lua/yvim/config/init.lua
+++ lua/yvim/config/init.lua
@@ -6,6 +6,40 @@ function M:load()
 
     vim.g.mapleader = " "
     vim.g.maplocalleader = ","
+
+    if vim.g.GuiLoaded then
+        vim.cmd([[
+          GuiTabline   0
+          GuiPopupmenu 0
+        ]])
+
+        -- Enable mouse
+        vim.opt.mouse = "a"
+        vim.keymap.set(
+            "n",
+            "<RightMouse>",
+            "<Cmd>call GuiShowContextMenu()<CR>",
+            { silent = true }
+        )
+        vim.keymap.set(
+            "i",
+            "<RightMouse>",
+            "<Cmd>call GuiShowContextMenu()<CR>",
+            { silent = true }
+        )
+        vim.keymap.set(
+            "x",
+            "<RightMouse>",
+            "<Cmd>call GuiShowContextMenu()<CR>",
+            { silent = true }
+        )
+        vim.keymap.set(
+            "s",
+            "<RightMouse>",
+            "<Cmd>call GuiShowContextMenu()<CR>",
+            { silent = true }
+        )
+    end
 end
 
 return M
