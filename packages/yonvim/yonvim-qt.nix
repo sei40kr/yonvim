a8d4305371c3ca9091c9e5c6eab415a6bab64542
Seong Yong-ju <sei40kr@gmail.com>
2022-07-24 03:56:59 +0900

    build: fix broken desktop item

--- packages/yonvim/yonvim-qt.nix
+++ packages/yonvim/yonvim-qt.nix
@@ -1,4 +1,5 @@
-{ makeWrapper
+{ makeDesktopItem
+, makeWrapper
 , neovim-qt
 , symlinkJoin
 , writeShellScriptBin
@@ -11,20 +12,49 @@ let
   neovim-qt-configured = neovim-qt.override {
     neovim = yonvim;
   };
-  yonvim-qt-bin = writeShellScriptBin "yvim-qt" ''
+  yvim-qt-bin = writeShellScriptBin "yvim-qt" ''
     exec ${neovim-qt-configured}/bin/nvim-qt --nvim ${yonvim}/bin/yvim -- "$@"
   '';
+  yvim-qt-desktop = makeDesktopItem {
+    name = "yvim-qt";
+    desktopName = "Yonvim";
+    comment = "Qt GUI for Neovim text editor";
+    icon = "nvim-qt";
+    exec = "yvim-qt -- %F";
+    mimeTypes = [
+      "text/english"
+      "text/plain"
+      "text/x-makefile"
+      "text/x-c++hdr"
+      "text/x-c++src"
+      "text/x-chdr"
+      "text/x-csrc"
+      "text/x-java"
+      "text/x-moc"
+      "text/x-pascal"
+      "text/x-tcl"
+      "text/x-tex"
+      "application/x-shellscript"
+      "text/x-c"
+      "text/x-c++"
+    ];
+    terminal = false;
+    categories = [ "Utility" "TextEditor" ];
+  };
 in
 symlinkJoin
 {
   name = "yonvim-qt-${version}";
 
-  paths = [ neovim-qt-configured ];
+  paths = [ neovim-qt-configured yvim-qt-bin ];
 
   nativeBuildInputs = [ makeWrapper ];
 
   postBuild = ''
     rm $out/bin/nvim-qt
-    cp ${yonvim-qt-bin}/bin/yvim-qt $out/bin/yvim-qt
+    rm -r $out/share/applications
+    mkdir $out/share/applications
+    cp ${yvim-qt-desktop}/share/applications/yvim-qt.desktop \
+       $out/share/applications
   '';
 }
