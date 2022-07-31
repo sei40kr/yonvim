e15276ad1a3ac6f5efd30458053d336c044a57c0
Seong Yong-ju <sei40kr@gmail.com>
2022-08-01 05:36:07 +0900

    fix(lsp): support lsp snippet

--- lua/yvim/plugin/lspconfig.lua
+++ lua/yvim/plugin/lspconfig.lua
@@ -6,6 +6,7 @@ function M.config()
     local capabilities = require("cmp_nvim_lsp").update_capabilities(
         vim.lsp.protocol.make_client_capabilities()
     )
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
     local opts = {
         on_attach = require("yvim.lsp").on_attach,
         capabilities = capabilities,
