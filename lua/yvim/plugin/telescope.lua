a0850993c648982afc0a4966df8b4d2183b2eb66
Seong Yong-ju <sei40kr@gmail.com>
2022-08-02 04:56:51 +0900

    refactor(ui): rewrite telescope border chars

--- lua/yvim/plugin/telescope.lua
+++ lua/yvim/plugin/telescope.lua
@@ -1,26 +1,31 @@
 local M = {}
 
-local function get_border_chars(border)
-    if border == "single" then
-        return { "─", "│", "─", "│", "┌", "┐", "┘", "└" }
-    elseif border == "double" then
-        return { "═", "║", "═", "║", "╔", "╗", "╝", "╚" }
-    elseif border == "rounded" then
-        return { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-    else
-        return { " ", " ", " ", " ", " ", " ", " ", " " }
-    end
+local function translate_border_chars(border_chars)
+    return {
+        border_chars[2],
+        border_chars[4],
+        border_chars[6],
+        border_chars[8],
+        border_chars[1],
+        border_chars[3],
+        border_chars[5],
+        border_chars[7],
+    }
 end
 
 function M.config()
+    local border_chars = require("yvim.util.ui").get_border_chars(
+        yvim.ui.border
+    )
+
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
     telescope.setup({
         defaults = {
             border = yvim.ui.border ~= "none",
-            borderchars = yvim.ui.border ~= "none" and get_border_chars(
-                yvim.ui.border
+            borderchars = yvim.ui.border ~= "none" and translate_border_chars(
+                border_chars
             ) or nil,
             mappings = {
                 i = {
