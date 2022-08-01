50871e773d547c76437232256300fd2c48741c10
Seong Yong-ju <sei40kr@gmail.com>
2022-08-02 01:41:31 +0900

    fix(tools): dedup neogit & octo config

--- lua/yvim/tools/init.lua
+++ lua/yvim/tools/init.lua
@@ -4,23 +4,10 @@ function M.setup() end
 
 function M.config()
     -- neogit
-    require("neogit").setup({
-        disable_hint = true,
-        disable_context_highlighting = true,
-        disable_commit_confirmation = true,
-        commit_popup = { kind = "tab" },
-        signs = {
-            section = { "", "" },
-            item = { "", "" },
-        },
-        integrations = { diffview = true },
-    })
+    require("yvim.plugin.neogit").config()
 
     -- octo.nvim
-    require("octo").setup({
-        right_bubble_delimiter = "",
-        left_bubble_delimiter = "",
-    })
+    require("yvim.plugin.octo").config()
 end
 
 return M
