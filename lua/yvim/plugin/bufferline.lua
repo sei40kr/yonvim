cbb0060cf56a0c0f30a9a776c669943ce90bd9dd
Seong Yong-ju <sei40kr@gmail.com>
2022-08-02 23:58:03 +0900

    feat(ui): use close-buffers.nvim for tab close

--- lua/yvim/plugin/bufferline.lua
+++ lua/yvim/plugin/bufferline.lua
@@ -3,6 +3,8 @@ local M = {}
 function M.config()
     require("bufferline").setup({
         options = {
+            close_command = 'lua require("yvim.core.buffer").delete_buffer()',
+            right_mouse_command = 'lua require("yvim.core.buffer").delete_buffer()',
             buffer_close_icon = "  ",
             modified_icon = "• ",
             indicator_icon = "▎ ",
