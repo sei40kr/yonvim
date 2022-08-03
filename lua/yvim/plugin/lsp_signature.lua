4bd5ff9cb5f23b396a1a2afd7ddd5dcd3df96310
Seong Yong-ju <sei40kr@gmail.com>
2022-08-04 02:33:57 +0900

    fix(lsp): window offset of signature help

--- lua/yvim/plugin/lsp_signature.lua
+++ lua/yvim/plugin/lsp_signature.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.config()
     require("lsp_signature").setup({
         bind = false,
+        floating_window_off_x = 0,
         fix_pos = false,
         hint_enable = false,
         handler_opts = {
