929bee252fd78a8c7fd87d0cb8cd66255e0d7a2d
Seong Yong-ju <sei40kr@gmail.com>
2022-08-06 01:58:39 +0900

    feat: implement keymaps.set_for_filetype

--- lua/yvim/util/keymaps.lua
+++ lua/yvim/util/keymaps.lua
@@ -1,7 +1,9 @@
 local Keymaps = {}
 
 Keymaps.init = function()
-    local keymaps = {}
+    local augroup = vim.api.nvim_create_augroup("yvim_keymaps", {})
+
+    local keymaps = { augroup = augroup }
     setmetatable(keymaps, { __index = Keymaps })
     return keymaps
 end
@@ -29,6 +31,16 @@ function Keymaps:set_leader_for_buffer(buffer, mode, keymaps)
         mode = mode,
         prefix = "<Leader>",
         buffer = buffer,
+    })
+end
+
+function Keymaps:set_for_filetype(filetype, mode, keymaps)
+    vim.api.nvim_create_autocmd("FileType", {
+        group = self.augroup,
+        pattern = filetype,
+        callback = function()
+            self:set_for_buffer(0, mode, keymaps)
+        end,
     })
 end
 
