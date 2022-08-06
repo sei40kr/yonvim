3f6dae2dc016a20161a99b6446b6d763ab86d26d
Seong Yong-ju <sei40kr@gmail.com>
2022-08-06 09:25:13 +0900

    feat(ui): disable lualine on special windows

--- lua/yvim/plugin/lualine/init.lua
+++ lua/yvim/plugin/lualine/init.lua
@@ -3,7 +3,16 @@ local M = {}
 function M.config()
     require("lualine").setup(vim.tbl_deep_extend("force", {
         options = {
-            disabled_filetypes = { "NvimTree" },
+            disabled_filetypes = {
+                "DiffviewFiles",
+                "help",
+                "man",
+                "NvimTree",
+                "Outline",
+                "qf",
+                "toggleterm",
+                "Trouble",
+            },
         },
     }, require("yvim.plugin.lualine.doom")))
 end
