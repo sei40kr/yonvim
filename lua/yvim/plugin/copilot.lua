5f83c8cfe000b557a2485928b7af6caafd7664b2
Seong Yong-ju <sei40kr@gmail.com>
2022-08-25 09:37:45 +0900

    fix(completion): defer copilot setup

--- lua/yvim/plugin/copilot.lua
+++ lua/yvim/plugin/copilot.lua
@@ -2,9 +2,9 @@ local M = {}
 
 function M.config()
     if yvim.completion.copilot.enable then
-        vim.schedule(function()
+        vim.defer_fn(function()
             require("copilot").setup()
-        end)
+        end, 100)
     end
 end
 
