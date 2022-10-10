ce83d1c683076899bad5091118b3a5b1cb9386cb
Seong Yong-ju <sei40kr@gmail.com>
2022-10-10 19:50:58 +0900

    refactor: remove empty modules

--- lua/yvim/bootstrap.lua
+++ lua/yvim/bootstrap.lua
@@ -5,27 +5,11 @@ function M.init()
 
     require("yvim.packer").init()
 
-    require("yvim.core").setup()
-    require("yvim.session").setup()
     require("yvim.treesitter").setup()
-    require("yvim.edit").setup()
     require("yvim.lsp").setup()
-    require("yvim.snippet").setup()
-    require("yvim.completion").setup()
-    require("yvim.terminal").setup()
-    require("yvim.tools").setup()
-    require("yvim.ui").setup()
 
-    require("yvim.core").config()
-    require("yvim.session").config()
     require("yvim.treesitter").config()
-    require("yvim.edit").config()
     require("yvim.lsp").config()
-    require("yvim.snippet").config()
-    require("yvim.completion").config()
-    require("yvim.terminal").config()
-    require("yvim.tools").config()
-    require("yvim.ui").config()
 
     require("yvim.core.keymap").load()
 end
--- lua/yvim/completion/init.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-function M.setup() end
-
-function M.config() end
-
-return M
