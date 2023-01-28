fde29e630288ef4f3d0d91d4852f1b8ef8023add
Seong Yong-ju <sei40kr@gmail.com>
2023-01-29 01:27:53 +0900

    build: support apple silicon

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     let
       inherit (nixpkgs) lib;
       inherit (lib) genAttrs;
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
       pkgs = genAttrs systems (system: import nixpkgs { inherit system; });
     in
     {
