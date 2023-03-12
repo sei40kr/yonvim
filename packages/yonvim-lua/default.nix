3534e4a5379834feb3d9ce1f5ee8ae4086a329a8
Seong Yong-ju <sei40kr@gmail.com>
2023-03-13 00:12:55 +0900

    fix(tree-sitter): use neovim compatible grammars

--- packages/yonvim-lua/default.nix
+++ packages/yonvim-lua/default.nix
@@ -1,55 +1,56 @@
 { lib, vimPlugins, vimUtils, yonvimPlugins }:
 
-let nvim-treesitter = vimPlugins.nvim-treesitter.withPlugins (ps: with ps; [
-  tree-sitter-bash
-  tree-sitter-c
-  tree-sitter-c-sharp
-  tree-sitter-cpp
-  tree-sitter-css
-  tree-sitter-dockerfile
-  tree-sitter-elisp
-  tree-sitter-fish
-  tree-sitter-go
-  tree-sitter-gomod
-  tree-sitter-graphql
-  tree-sitter-haskell
-  tree-sitter-hcl
-  tree-sitter-html
-  tree-sitter-java
-  tree-sitter-javascript
-  tree-sitter-jsdoc
-  tree-sitter-json
-  tree-sitter-json5
-  tree-sitter-julia
-  tree-sitter-kotlin
-  tree-sitter-lua
-  tree-sitter-make
-  tree-sitter-markdown
-  tree-sitter-markdown-inline
-  tree-sitter-nix
-  tree-sitter-norg
-  tree-sitter-ocaml
-  tree-sitter-perl
-  tree-sitter-php
-  tree-sitter-python
-  tree-sitter-r
-  tree-sitter-regex
-  tree-sitter-ruby
-  tree-sitter-rust
-  tree-sitter-scala
-  tree-sitter-scss
-  tree-sitter-sql
-  tree-sitter-svelte
-  # tree-sitter-swift
-  tree-sitter-toml
-  tree-sitter-tsx
-  tree-sitter-typescript
-  tree-sitter-verilog
-  tree-sitter-vim
-  tree-sitter-vue
-  tree-sitter-yaml
-  tree-sitter-zig
-]);
+let
+  nvim-treesitter = vimPlugins.nvim-treesitter.withPlugins (ps: with ps; [
+    bash
+    c
+    c-sharp
+    cpp
+    css
+    dockerfile
+    elisp
+    fish
+    go
+    gomod
+    graphql
+    haskell
+    hcl
+    html
+    java
+    javascript
+    jsdoc
+    json
+    json5
+    julia
+    kotlin
+    lua
+    make
+    markdown
+    markdown-inline
+    nix
+    norg
+    ocaml
+    perl
+    php
+    python
+    r
+    regex
+    ruby
+    rust
+    scala
+    scss
+    sql
+    svelte
+    # swift
+    toml
+    tsx
+    typescript
+    verilog
+    vim
+    vue
+    yaml
+    zig
+  ]);
 in
 vimUtils.buildVimPluginFrom2Nix {
   pname = "yonvim.lua";
