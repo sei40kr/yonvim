e4b80e514934bc16556184e14629d442a4091779
Seong Yong-ju <sei40kr@gmail.com>
2023-04-18 01:07:14 +0900

    feat(treesitter): disable slow highlight for large files

--- lua/yvim/plugins/treesitter.lua
+++ lua/yvim/plugins/treesitter.lua
@@ -10,7 +10,16 @@ return {
         },
         opts = {
             endwise = { enable = true },
-            highlight = { enable = true },
+            highlight = {
+                enable = true,
+                disable = function(_lang, buf)
+                    local max_filesize = 100 * 1024
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+            },
             indent = { enable = true },
             incremental_selection = {
                 enable = true,
