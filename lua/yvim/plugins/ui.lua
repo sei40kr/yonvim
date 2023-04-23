021697bd6af47d5c2a80ce92d2b9881be3185c40
Seong Yong-ju <sei40kr@gmail.com>
2023-04-23 18:25:35 +0900

    feat(ui): thicken gitsigns' gutter

--- lua/yvim/plugins/ui.lua
+++ lua/yvim/plugins/ui.lua
@@ -63,36 +63,12 @@ return {
         dir = "@gitsigns@",
         opts = {
             signs = {
-                add = {
-                    hl = "GitSignsAdd",
-                    text = "▎",
-                    numhl = "GitSignsAddNr",
-                    linehl = "GitSignsAddLn",
-                },
-                change = {
-                    hl = "GitSignsChange",
-                    text = "▎",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
-                },
-                delete = {
-                    hl = "GitSignsDelete",
-                    text = "▸",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
-                },
-                topdelete = {
-                    hl = "GitSignsDelete",
-                    text = "▸",
-                    numhl = "GitSignsDeleteNr",
-                    linehl = "GitSignsDeleteLn",
-                },
-                changedelete = {
-                    hl = "GitSignsChange",
-                    text = "▎",
-                    numhl = "GitSignsChangeNr",
-                    linehl = "GitSignsChangeLn",
-                },
+                add = { text = "▍" },
+                change = { text = "▍" },
+                delete = { text = "▸" },
+                topdelete = { text = "▸" },
+                changedelete = { text = "▍" },
+                untracked = { text = "▍" },
             },
             on_attach = function(bufnr)
                 local map = function(mode, lhs, rhs, opts)
