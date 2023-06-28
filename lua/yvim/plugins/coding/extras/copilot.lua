8a125b3cf58df8ddead0f9343edf5a4abe307bdc
Seong Yong-ju <sei40kr@gmail.com>
2023-06-29 00:42:54 +0900

    refactor(copilot): remove deprecated plugin options

--- lua/yvim/plugins/coding/extras/copilot.lua
+++ lua/yvim/plugins/coding/extras/copilot.lua
@@ -19,15 +19,8 @@ return {
                 name = "copilot_cmp",
                 lazy = true,
                 dependencies = { "copilot" },
-                opts = function()
-                    return {
-                        formatters = {
-                            insert_text = require("copilot_cmp.format").remove_existing,
-                        },
-                    }
-                end,
-                config = function(_, opts)
-                    require("copilot_cmp").setup(opts)
+                config = function()
+                    require("copilot_cmp").setup()
 
                     local cmp = require("cmp")
                     cmp.event:on("menu_opened", function()
