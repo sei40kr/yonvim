e1eefdaef274bd1e7cef1e9ea3708b1521d0ae1f
Seong Yong-ju <sei40kr@gmail.com>
2023-10-07 21:24:18 +0900

    feat(coding): bump friendly-snippets
    rafamadriz/friendly-snippets@ad7cef5d538b023ee3af0fe3170cadb096171b1f -> rafamadriz/friendly-snippets@43727c2ff84240e55d4069ec3e6158d74cb534b6

--- packages/yonvim/plugins/generated.nix
+++ packages/yonvim/plugins/generated.nix
@@ -324,13 +324,13 @@ _self: {
 
   friendly-snippets = vimUtils.buildVimPlugin {
     pname = "friendly-snippets";
-    version = "2023-07-01";
+    version = "2023-10-01";
 
     src = fetchFromGitHub {
       owner = "rafamadriz";
       repo = "friendly-snippets";
-      rev = "ad7cef5d538b023ee3af0fe3170cadb096171b1f";
-      hash = "sha256-yRARQn70R/8drOkPzUaBUIa0Ka62r3wNhxvdPNHhLuU=";
+      rev = "43727c2ff84240e55d4069ec3e6158d74cb534b6";
+      hash = "sha256-qKY2UEUToOLkAgTNvKb7SSstOX/KydkIUiqFZN8JU+o=";
     };
 
     dontBuild = true;
