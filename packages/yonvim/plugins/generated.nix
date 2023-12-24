687b545b882220a7666fea1e19177e822aad7a73
Seong Yong-ju <sei40kr@gmail.com>
2023-12-24 20:50:15 +0900

    feat(editor): bump nvim-window-picker
    s1n7ax/nvim-window-picker@e7b6699fbd007bbe61dc444734b9bade445b2984 -> s1n7ax/nvim-window-picker@41cfaa428577c53552200a404ae9b3a0b5719706

--- packages/yonvim/plugins/generated.nix
+++ packages/yonvim/plugins/generated.nix
@@ -868,13 +868,13 @@ _self: {
 
   nvim-window-picker = vimUtils.buildVimPlugin {
     pname = "nvim-window-picker";
-    version = "2023-09-24";
+    version = "2023-12-17";
 
     src = fetchFromGitHub {
       owner = "s1n7ax";
       repo = "nvim-window-picker";
-      rev = "e7b6699fbd007bbe61dc444734b9bade445b2984";
-      hash = "sha256-IpaWJMZH5TUtQhQZzzSA35liZhZ2YMrJm2E/6hdohQY=";
+      rev = "41cfaa428577c53552200a404ae9b3a0b5719706";
+      hash = "sha256-D5ikm5Fw0N/AjDL71cuATp1OCONuxPZNfEHuh0vXkq0=";
     };
 
     dontBuild = true;
