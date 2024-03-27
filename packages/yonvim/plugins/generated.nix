95156f14c3db8cc6fbe54829dad8cd6dfa82fe88
Seong Yong-ju <yongju.seong@codetakt.com>
2024-03-27 23:27:18 +0900

    feat(ui): bump dressing.nvim
    stevearc/dressing.nvim@6f212262061a2120e42da0d1e87326e8a41c0478 -> stevearc/dressing.nvim@18e5beb3845f085b6a33c24112b37988f3f93c06

--- packages/yonvim/plugins/generated.nix
+++ packages/yonvim/plugins/generated.nix
@@ -276,13 +276,13 @@ _self: {
 
   dressing-nvim = vimUtils.buildVimPlugin {
     pname = "dressing.nvim";
-    version = "2024-01-29";
+    version = "2024-03-13";
 
     src = fetchFromGitHub {
       owner = "stevearc";
       repo = "dressing.nvim";
-      rev = "6f212262061a2120e42da0d1e87326e8a41c0478";
-      hash = "sha256-Y+ABLhb3GIaPKOuQzkxsZsTo1WfgURAYVioP/eCSp/Y=";
+      rev = "18e5beb3845f085b6a33c24112b37988f3f93c06";
+      hash = "sha256-9AwOFTRvhWFo7USgoFYfceiojZM62IXPpBs8CnSqc18=";
     };
 
     dontBuild = true;
