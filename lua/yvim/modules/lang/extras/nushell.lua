8ddeb5e9750a453bdd8705b4d118129947ceb42a
Seong Yong-ju <yongju.seong@codetakt.com>
2024-05-30 22:38:04 +0900

    fix(nushell): set filetype for `*.nu` files

--- lua/yvim/modules/lang/extras/nushell.lua
+++ lua/yvim/modules/lang/extras/nushell.lua
@@ -1,6 +1,13 @@
 return {
     {
         "nvim-nu",
+        init = function()
+            vim.filetype.add({
+                extension = {
+                    nu = "nu",
+                },
+            })
+        end,
         opts = { use_lsp_features = false },
         ft = { "nu" },
     },
